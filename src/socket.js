73d16fe025f5725ae573fac197eed58f28bc81aa
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-01-10 00:28:59 +0700

    update client

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import {io} from "socket.io-client";
-export const socket = io("https://be-electro-api.onrender.com", {
+export const socket = io("https://be-electro-api.onrender.com/api", {
     autoConnect: false
 });
