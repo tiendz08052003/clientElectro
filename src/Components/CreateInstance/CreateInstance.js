cf534cc17b8fce381236f0821790170966609a8d
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-01-10 00:00:22 +0700

    update client

--- src/Components/CreateInstance/CreateInstance.js
+++ src/Components/CreateInstance/CreateInstance.js
@@ -4,7 +4,7 @@ import * as AuthServices  from "~/services/AuthServices";
 
 export const CreateAxios = (user, dispatch, stateSuccess) => {
     const newInstance = axios.create({
-        baseURL: "https://be-electro-api.onrender.com/",
+        baseURL: "https://be-electro-api.onrender.com/api/",
         withCredentials: true,
     })
 
