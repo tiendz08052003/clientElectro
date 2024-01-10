f7c2858137f999865acdd01484d909fd72b7a97f
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-01-10 23:22:25 +0700

    update client

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -32,10 +32,10 @@ function Chat() {
     const [listMessageChat, setListMessageChat] = useState([]);
     const [showSidebar, setShowSidebar] = useState(1);
     const [valueInput, setValueInput] = useState("");
+    const [htmlContent, setHtmlContent] = useState("");
     const [collapse, setCollapse] = useState(true);
     const ref = useRef(null);
     const refMainMessage = useRef(null);
-    let htmlContent = "";
     socket.connect();
 
     const User = useSelector(getUser);
@@ -129,10 +129,16 @@ function Chat() {
         socket.on("sever-send-client-message", (data) => {
             setMessageSummary(data.messageInput)
             if(data.User._id === User._id)
-                htmlContent +=  `<div class="chat__main__bottom__content__main__div" style="justify-content: right;"><span class="chat__main__bottom__content__main__div__content" style="background-color: rgb(0, 132, 255);">${data.messageInput}</span></div>`
+            {
+                let x = htmlContent + `<div class="chat__main__bottom__content__main__div" style="justify-content: right;"><span class="chat__main__bottom__content__main__div__content" style="background-color: rgb(0, 132, 255);">${data.messageInput}</span></div>`
+                setHtmlContent(x)
+            }
             else
+            {
                 // eslint-disable-next-line no-unused-vars
-                htmlContent += `<div class="chat__main__bottom__content__main__div"><span class="chat__main__bottom__content__main__div__content">${data.messageInput}</span></div>`
+                let x = htmlContent + `<div class="chat__main__bottom__content__main__div"><span class="chat__main__bottom__content__main__div__content">${data.messageInput}</span></div>`
+                setHtmlContent(x)
+            }
         })
 
         socket.on("sever-send-client-finishChat", () => {
