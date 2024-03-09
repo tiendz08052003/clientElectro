c557b5ef2d32df649e090df7202846203fb88d27
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-03-09 22:04:19 +0700

    update client

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,7 +20,7 @@ const persistConfig = {
     key: 'root',
     version: 1,
     storage,
-    whitelist: ["account", "chat"]
+    whitelist: ["account", "header", "chat"]
   }
 
 const rootReducer = combineReducers({
