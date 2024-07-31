d0cd311d55329e5f052143a06a5ba4a2e6752e41
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-07-31 08:45:29 +0700

    update client

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 export const socket = io("https://be-electro-api.onrender.com/api", {
     autoConnect: false,
     withCredentials: true,
+    transports: ['websocket', 'polling'],
     extraHeaders: {
       "my-custom-header": "abcd"
     }
